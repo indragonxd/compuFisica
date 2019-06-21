9fe400dcd5f22f14f49cf6dad20c61ab6557b440
sherwin <sherwin.marquez@unmsm.edu.pe>
2019-06-21 17:03:10 -0500

    init

---
+++ src/views/About.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          <h1>coms</h1>
+        </div>
+        <div class="card-body">
+          <table class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Dia</th>
+                <th>Consumo</th>
+                <th>Precio</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="com in coms" :key="com['.key']">
+                <td>
+                  <a :ref="com.dni" target="_blank">{{com.nombre}}</a>
+                </td>
+                <td>
+                  {{com.direccion}}
+                  <!--- {{com['.key']}}-->
+                </td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import firebase from "firebase";
+import config from "@/config.js";
+
+let ap = firebase.initializeApp(config);
+let db = ap.database();
+let comsiteRef = db.ref("consumo");
+
+export default {
+  name : "about",
+  data() {
+    return {
+      coms: [],
+      app : {},
+      idUser : ''
+    };
+
+  },
+
+  created() {
+    /*this.app = route.params.ap;
+    this.idUser = $route.params.uss;
+    
+    console.log(this.idUser);
+    */
+    comsiteRef.on("value", snapchat => {
+      this.coms = [];
+      let valores = snapchat.val();
+      for (let val in valores) {
+        this.coms.push({
+          ".key": val,
+          nombre: valores[val].nombre,
+          direccion: valores[val].direccion,
+          dni: valores[val].dni
+        });
+      }
+    });
+  }
+};
+</script>
